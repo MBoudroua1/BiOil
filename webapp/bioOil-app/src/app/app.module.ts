c3e16ca441f5a8a061d43c764bd65dd375a957dc
MKB <mboudroua95@gmail.com>
2019-07-23 19:47:57 +0200

    component dashboard updated

--- webapp/bioOil-app/src/app/app.module.ts
+++ webapp/bioOil-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponentComponent } from './Dashboard/dashboard-component/dashboard-component.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { OrderComponent } from './order/order.component';
 import { GoodsComponent } from './goods/goods.component';
@@ -11,7 +10,6 @@ import { GoodsComponent } from './goods/goods.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponentComponent,
     DashboardComponent,
     OrderComponent,
     GoodsComponent
