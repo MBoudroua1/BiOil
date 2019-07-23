af65be3f2611e890d1a30db3cbaf164bd8d0f9c1
MKB <mboudroua95@gmail.com>
2019-07-23 19:21:04 +0200

    commit new component

--- webapp/bioOil-app/src/app/app.module.ts
+++ webapp/bioOil-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponentComponent } from './Dashboard/dashboard-component/dashboard-component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrderComponent } from './order/order.component';
+import { GoodsComponent } from './goods/goods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponentComponent,
+    DashboardComponent,
+    OrderComponent,
+    GoodsComponent
   ],
   imports: [
     BrowserModule,
