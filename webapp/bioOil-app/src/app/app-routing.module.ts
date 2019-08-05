6b0ed25c368a77b9a727a2789e5ae9de204ba191
MKB <mboudroua95@gmail.com>
2019-08-05 23:50:46 +0200

    update on templates footer and header

--- webapp/bioOil-app/src/app/app-routing.module.ts
+++ webapp/bioOil-app/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoodsComponent } from './goods/goods.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'goods', component: GoodsComponent },
+  { path: 'order', component: DashboardComponent },
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
