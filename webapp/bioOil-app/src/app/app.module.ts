e74fcff05fe5992a27da50db9dd6648093c4ee28
MKB <mboudroua95@gmail.com>
2019-08-05 22:04:20 +0200

    footer component updated

--- webapp/bioOil-app/src/app/app.module.ts
+++ webapp/bioOil-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { OrderComponent } from './order/order.component';
 import { GoodsComponent } from './goods/goods.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
