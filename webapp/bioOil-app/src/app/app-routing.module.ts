f68f5bb10f16514c8275efa34bf3f143f2a0d102
Boudroua <mboudroua95@gmail.com>
2019-08-12 01:20:12 +0200

    pro and contact component add

--- webapp/bioOil-app/src/app/app-routing.module.ts
+++ webapp/bioOil-app/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GoodsComponent } from './goods/goods.component';
+import { ProComponent } from './pro/pro.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
@@ -9,6 +11,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'goods', component: GoodsComponent },
   { path: 'order', component: DashboardComponent },
+  { path: 'pro', component: ProComponent },
+  { path: 'contactUs', component: ContactComponent}
   
   
 ];
