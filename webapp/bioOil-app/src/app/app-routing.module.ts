9e029a5d4f3c95ce2a55bf037c7458c6f720fd3d
MKB <mboudroua95@gmail.com>
2019-08-12 10:41:23 +0200

    commit after pull

--- webapp/bioOil-app/src/app/app-routing.module.ts
+++ webapp/bioOil-app/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GoodsComponent } from './goods/goods.component';
+import { OrderComponent } from './order/order.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'goods', component: GoodsComponent },
-  { path: 'order', component: DashboardComponent },
+  { path: 'order', component: OrderComponent },
   
   
 ];
